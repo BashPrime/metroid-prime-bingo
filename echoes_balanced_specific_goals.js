729787836b731f9f1171ba469f7e1ba15c186efa
BashPrime <5769306+BashPrime@users.noreply.github.com>
2021-06-15 00:17:09 -0700

    New goal sheet

---
+++ echoes_balanced_specific_goals.js
@@ -0,0 +1,74 @@
+var bingoList = [];
+
+bingoList[1] = [
+  { name: 'Missile Launcher', types: ['progression', 'missile'] },
+  { name: 'Violet Translator', types: ['progression', 'translator'] },
+  { name: 'Destroy the wall in Sacred Path', types: ['temple-grounds', 'event'] },
+  { name: 'Morph Ball Bomb', types: ['progression', 'morph'] },
+  { name: 'Space Jump Boots', types: ['progression', 'movement'] }
+];
+bingoList[2] = [
+  { name: 'Defeat both Sandiggers in Mining Plaza', types: ['agon', 'enemy'] },
+  { name: 'Talk to I-Sha', types: ['agon', 'event'] }
+];
+bingoList[3] = [
+  { name: 'Open the gate in Transport Center', types: ['agon', 'event'] },
+  { name: 'Defeat Jump Guardian', types: ['agon', 'sub-guardian'] },
+  { name: 'Defeat Dark Missile Trooper', types: ['temple-grounds', 'minor-boss'] }
+];
+bingoList[4] = [
+  { name: 'Storage D item', types: ['agon'] },
+  { name: 'Ing Cache 1 item', types: ['agon'] }
+]
+bingoList[6] = [
+  { name: 'Defeat Amorbis', types: ['agon', 'guardian'] },
+  { name: 'Dark Suit', types: ['progression'] },
+  { name: 'Super Missile', types: ['progression'] }
+];
+bingoList[7] = [
+  { name: 'Move the Forgotten Bridge', types: ['torvus', 'event'] },
+  { name: 'Enter 2 Generator Portals', types: ['portal'] },
+  { name: 'Talk to A-Voq', types: ['torvus', 'event' ]}
+];
+bingoList[11] = [
+  { name: 'Grapple Beam', types: ['progression', 'movement'] },
+  { name: 'Gravity Boost', types: ['progression', 'movement'] },
+  { name: 'Defeat Alpha Blogg', types: ['torvus', 'minor-boss'] }
+];
+bingoList[12] = [
+  { name: 'Defeat Chykka', types: ['torvus', 'guardian'] },
+  { name: 'Boost Ball', types: ['progression', 'morph'] },
+  { name: 'Seeker Missile', types: ['progression', 'missile'] }
+];
+bingoList[15] = [
+  { name: 'Dark Visor', types: ['progression', 'visor'] },
+  { name: 'Echo Visor', types: ['progression', 'visor'] }
+];
+bingoList[20] = [
+  { name: 'Destroy the Sanctuary Entrance bridge', types: ['sanctuary', 'event'] }
+]
+bingoList[21] = [
+  { name: 'Defeat Caretaker Drone', types: ['sanctuary', 'minor-boss'] },
+  { name: 'Aerie item', types: ['sanctuary'] },
+  { name: 'Meditation Vista item', types: ['torvus'] },
+  { name: 'Defeat a Dark War Wasp', types: ['enemy'] }
+];
+bingoList[22] = [
+  { name: 'Storage A item', types: ['agon'] },
+  { name: 'Sandcanyon item', types: ['agon'] },
+  { name: 'Vault item', types: ['sanctuary'] },
+  { name: 'Feeding Pit item', types: ['agon', 'light-suit'] }
+];
+bingoList[23] = [
+  { name: 'Defeat Quadraxis', types: ['sanctuary', 'guardian'] }
+];
+bingoList[24] = [
+  { name: 'Poisoned Bog item', type: ['torvus', 'light-suit', 'flying-ing-cache'] },
+  { name: 'Dungeon item', type: ['torvus', 'light-suit', 'flying-ing-cache'] },
+  { name: 'Ing Reliquary item', type: ['temple-grounds', 'light-suit', 'flying-ing-cache'] },
+];
+bingoList[25] = [
+  { name: 'Main Energy Controller (Light Suit) item', type: ['u-mos'] },
+  { name: 'Dark Oasis item', type: ['agon', 'light-suit', 'flying-ing-cache'] },
+  { name: 'Hive Entrance item', type: ['sanctuary', 'light-suit', 'flying-ing-cache'] }
+];
